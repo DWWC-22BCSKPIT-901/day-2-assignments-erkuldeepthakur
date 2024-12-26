e2f8a4915f59867afaf48133645b37ba4114c837
Kuldeep Thakur <151065929+erkuldeepthakur@users.noreply.github.com>
2024-12-26 09:41:34 +0530

    Update Very easy.cpp

--- day  2/Very easy.cpp
+++ day  2/Very easy.cpp
@@ -1,1 +1,24 @@
+#include <vector>
+#include <iostream>
+using namespace std;
 
+int majorityElement(vector<int>& nums) {
+    int count = 0, candidate = 0;
+    for (int num : nums) {
+        if (count == 0) {
+            candidate = num;
+        }
+        count += (num == candidate) ? 1 : -1;
+    }
+    return candidate;
+}
+
+int main() {
+    vector<int> nums1 = {3, 2, 3};
+    cout << "Majority Element: " << majorityElement(nums1) << endl;
+
+    vector<int> nums2 = {2, 2, 1, 1, 1, 2, 2};
+    cout << "Majority Element: " << majorityElement(nums2) << endl;
+
+    return 0;
+}
